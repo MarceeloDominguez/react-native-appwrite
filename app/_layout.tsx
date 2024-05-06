ba457418a95b0ed112610723ec299c023f7d5ee7
marcelo dominguez <marceelodominguez@gmail.com>
2024-05-06 17:13:58 -0300

    configuring fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    PoppinsBlack: require("../assets/fonts/Poppins-Black.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsSemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" />
--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function index() {
   return (
     <View>
-      <Text>index</Text>
+      <Text style={{ fontFamily: "PoppinsSemiBold" }}>index</Text>
     </View>
   );
 }
